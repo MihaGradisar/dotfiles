5d4543b3144aae0d4209abc67088ee98573302e6
Miha Gradišar <gradisar.mih4@gmail.com>
2026-03-11 13:56:02 +0100

    nvim color update

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,12 +1,20 @@
 return {
-	"vague-theme/vague.nvim",
+	"oskarnurm/koda.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("vague").setup({
+		require("koda").setup({
 			transparent = true,
-			italic = false,
+			colors = {
+				keyword = "#cc8bc9",
+				type = "#cecece",
+				char = "#95cb82",
+			},
 		})
-		vim.cmd("colorscheme vague")
+		vim.cmd("colorscheme koda-moss")
+		vim.api.nvim_set_hl(0, "Delimiter", { fg = "#708b8d" })
+		vim.api.nvim_set_hl(0, "@tag", { fg = "#cecece" })
+		vim.api.nvim_set_hl(0, "@tag.delimiter", { fg = "#708b8d" })
 	end,
 }
+
